ec71e7d1e2105926c27c607338584a671bb69b9f
hector <hlinares@ecomotriz.com>
2019-09-16 19:28:35 +0200

    Created a middleware in nodejs to access node from PHP Added entity game to the server database Started some server game download process, won't be completed for the deadline

---
+++ server/src/Entity/Game.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\GameRepository")
+ */
+class Game
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=32)
+     */
+    private $uuid;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $url;
+
+    /**
+     * @ORM\Column(type="string", length=128)
+     */
+    private $contractAddress;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUuid(): ?string
+    {
+        return $this->uuid;
+    }
+
+    public function setUuid(string $uuid): self
+    {
+        $this->uuid = $uuid;
+
+        return $this;
+    }
+
+    public function getUrl(): ?string
+    {
+        return $this->url;
+    }
+
+    public function setUrl(string $url): self
+    {
+        $this->url = $url;
+
+        return $this;
+    }
+
+    public function getContractAddress(): ?string
+    {
+        return $this->contractAddress;
+    }
+
+    public function setContractAddress(string $contractAddress): self
+    {
+        $this->contractAddress = $contractAddress;
+
+        return $this;
+    }
+}
